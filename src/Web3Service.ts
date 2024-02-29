3d4c0d4a4c5e0b77e22b89f0e7a6c0c37f1f616d
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-02-29 06:47:49 -0300

    feat: add mint function

--- src/Web3Service.ts
+++ src/Web3Service.ts
@@ -1,4 +1,7 @@
 import Web3 from "web3"
+import ABI from "./abi.json"
+
+const CONTRACT_ADDRESS = `${process.env.REACT_APP_CONTRACT_ADDRESS}`
 
 async function mint() {
   if (!window.ethereum) throw new Error("No MetaMask was found!")
@@ -7,7 +10,13 @@ async function mint() {
   const accounts = await web3.eth.requestAccounts()
   if (!accounts || !accounts.length) throw new Error("No account allowed")
 
-  return accounts[0]
+  const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS, {
+    from: accounts[0],
+  })
+  const tx = await contract.methods.mint().send()
+
+  console.log(tx.transactionHash)
+  return tx.transactionHash
 }
 
 export { mint }
